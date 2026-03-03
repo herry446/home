e2e74be82d5e899ceddf51c3ad40e8ad9c5247b9
herry446 <mrchekin12345ggsel123@gmail.com>
2026-03-03 10:03:46 +0600

    index.html

---
+++ app.js
@@ -0,0 +1,10 @@
+//! Massiv / Array
+let name = "Baitur"; //! Переменная
+//? index       0        1         2
+let Array = ["Baitur", "Dastan", "Aibek"]; //! Массив
+console.log(Array[0]); //! Выводим элемент массива по индексу
+
+let city = ["Bishkek", "Osh", "Jalal-Abad", "Naryn", "bursh-halifa"];
+console.log(city);
+console.log(city.length);
+console.log(city[5]);
